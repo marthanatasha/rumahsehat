a546d95f39461b9c4ba3f338037a05ecd6b07786
Reyhan Ariq Syahalam <ariqsyahalam@risya.id>
2022-11-18 07:17:51 +0700

    Getting data is done

--- rumahsehat/src/main/java/apap/tugas/akhir/rumahsehat/controller/BaseController.java
+++ rumahsehat/src/main/java/apap/tugas/akhir/rumahsehat/controller/BaseController.java
@@ -1,12 +1,19 @@
 package apap.tugas.akhir.rumahsehat.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import apap.tugas.akhir.rumahsehat.service.ObatService;
+
 @Controller
 public class BaseController {
+    @Autowired
+    ObatService obatService;
+
     @GetMapping("/")
     private String Home() {
+        System.out.println(obatService.getListObat());
         return "home";
     }
 }
