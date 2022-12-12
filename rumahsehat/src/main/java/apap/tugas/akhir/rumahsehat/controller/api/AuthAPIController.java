63a29f877fd3e9c5ff871b31c0ea41d2163afb8c
Reyhan Ariq Syahalam <ariqsyahalam@risya.id>
2022-12-12 22:13:05 +0700

    fixing

--- rumahsehat/src/main/java/apap/tugas/akhir/rumahsehat/controller/api/AuthAPIController.java
+++ rumahsehat/src/main/java/apap/tugas/akhir/rumahsehat/controller/api/AuthAPIController.java
@@ -1,5 +1,7 @@
 package apap.tugas.akhir.rumahsehat.controller.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import apap.tugas.akhir.rumahsehat.controller.web.AppointmentController;
 import apap.tugas.akhir.rumahsehat.model.DTO.LoginAPIDTO;
 import apap.tugas.akhir.rumahsehat.model.users.UserModel;
 import apap.tugas.akhir.rumahsehat.service.AuthService;
@@ -24,22 +27,29 @@ public class AuthAPIController {
 
     @Autowired
     AuthService authService;
+    
+    Logger logger = LoggerFactory.getLogger(AuthAPIController.class);
+
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginAPIDTO loginAPIDTO, BindingResult bindingResult) {
         String token = authService.getToken(loginAPIDTO);
 
         if (token == null) {
+            logger.info("API POST: Token Invalid");
             return new ResponseEntity<String>("Unauthorized", HttpStatus.UNAUTHORIZED);
         }
+        logger.info("API POST: Authentication Valid");
         return new ResponseEntity<>(authService.getToken(loginAPIDTO), HttpStatus.OK);
     }
 
     @GetMapping("/info")
     public ResponseEntity<?> getUserInfo(@RequestHeader("Authorization") String bearerToken, Model model) {
         if (!authService.tokenCheck(bearerToken)) {
+            logger.info("API POST: Token Invalid");
             return new ResponseEntity<String>("Unauthorized", HttpStatus.UNAUTHORIZED);
         }
+        logger.info("API GET: Info user");
         return new ResponseEntity<>(authService.getUserInfo(bearerToken), HttpStatus.OK);
     }
 }
