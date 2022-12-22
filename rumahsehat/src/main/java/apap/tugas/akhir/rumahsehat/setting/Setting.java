f6db5ffedfe03d001e00829289b716cb8a74a463
salma.karimah <salma.karimah@ui.ac.id>
2022-12-22 18:57:17 +0700

    Deployment: image update

--- rumahsehat/src/main/java/apap/tugas/akhir/rumahsehat/setting/Setting.java
+++ rumahsehat/src/main/java/apap/tugas/akhir/rumahsehat/setting/Setting.java
@@ -1,17 +1,17 @@
 package apap.tugas.akhir.rumahsehat.setting;
 
 public class Setting {
-    public static final String CLIENT_BASE_URL = "https://apap-070.cs.ui.ac.id";
+    final public static String CLIENT_BASE_URL = "https://apap-070.cs.ui.ac.id";
 
-    public static final String CLIENT_LOGIN = CLIENT_BASE_URL + "/validate-ticket";
+    final public static String CLIENT_LOGIN = CLIENT_BASE_URL + "/validate-ticket";
 
-    public static final String CLIENT_LOGOUT = CLIENT_BASE_URL + "/logout";
+    final public static String CLIENT_LOGOUT = CLIENT_BASE_URL + "/logout";
 
-    public static final String SERVER_BASE_URL = "https://sso.ui.ac.id/cas2";
+    final public static String SERVER_BASE_URL = "https://sso.ui.ac.id/cas2";
 
-    public static final String SERVER_LOGIN = SERVER_BASE_URL + "/login?service=";
+    final public static String SERVER_LOGIN = SERVER_BASE_URL + "/login?service=";
 
-    public static final String SERVER_LOGOUT = SERVER_BASE_URL + "/logout?url=";
+    final public static String SERVER_LOGOUT = SERVER_BASE_URL + "/logout?url=";
 
-    public static final String SERVER_VALIDATE_TICKET = SERVER_BASE_URL + "/serviceValidate?ticket=%s&service=%s";
+    final public static String SERVER_VALIDATE_TICKET = SERVER_BASE_URL + "/serviceValidate?ticket=%s&service=%s";
 }
